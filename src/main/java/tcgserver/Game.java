fd3a6326b6622058c895295882c249dabe41f1dc
azakhi <azak.halilibrahim@gmail.com>
2019-06-17 21:59:05 +0300

    Add adding player tests

--- src/main/java/tcgserver/Game.java
+++ src/main/java/tcgserver/Game.java
@@ -1,5 +1,39 @@
 package tcgserver;
 
-public class Game {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class Game {
+    public enum GameState {
+        INITIAL,
+        ACTIVE,
+        END
+    }
+
+    public final static int MAX_PLAYERS = 2;
+
+    private GameState state;
+    private ArrayList<Player> players;
+    private ArrayList<Card> initialDeck;
+
+    public Game() {
+        this(Arrays.asList(new Card(0), new Card(1), new Card(2), new Card(3), new Card(4)));
+    }
+
+    public Game(List<Card> initialDeck) {
+        state = GameState.INITIAL;
+        players = new ArrayList<>();
+        this.initialDeck = new ArrayList<>();
+        this.initialDeck.addAll(initialDeck);
+    }
+
+    public List<Player> getPlayers() {
+        return Collections.unmodifiableList(players);
+    }
+
+    public Player addPlayer(User user) {
+        return new Player(Collections.emptyList(), Collections.emptyList());
+    }
 }
--- src/test/java/tcgserver/GameTest.java
+++ src/test/java/tcgserver/GameTest.java
@@ -1,8 +1,11 @@
 package tcgserver;
 
 import org.junit.After;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,5 +17,36 @@ public class GameTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addPlayer_LessThanMaxPlayers_PlayerAdded() {
+        Assume.assumeTrue(Game.MAX_PLAYERS > 0);
+
+        // Arrange
+        Game game = new Game(Arrays.asList(new Card(0), new Card(1), new Card(2), new Card(3), new Card(4)));
+
+        // Act
+        Player player = game.addPlayer(new User());
+
+        // Assert
+        assertEquals(1, game.getPlayers().size());
+        assertEquals(player, game.getPlayers().get(0));
+        assertEquals(5, player.getDeck().size());
+    }
+
+    @Test
+    public void addPlayer_MaxPlayers_PlayerNotAdded() {
+        // Arrange
+        Game game = new Game();
+        for (int i = 0; i < Game.MAX_PLAYERS; i++) game.addPlayer(new User());
+        Assume.assumeTrue(game.getPlayers().size() == Game.MAX_PLAYERS);
+
+        // Act
+        Player player = game.addPlayer(new User());
+
+        // Assert
+        assertEquals(Game.MAX_PLAYERS, game.getPlayers().size());
+        assertNull(player);
     }
 }
