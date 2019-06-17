1004907acb1bebc864bf7f1b7d886ff1dd12c9a9
azakhi <azak.halilibrahim@gmail.com>
2019-06-17 21:16:02 +0300

    Use JUnit 5 instead of 4

--- src/test/java/tcgserver/PlayerTest.java
+++ src/test/java/tcgserver/PlayerTest.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
 
