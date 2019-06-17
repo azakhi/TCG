a066a1763300e9a395189a1e1d328527cd05d4ee
azakhi <azak.halilibrahim@gmail.com>
2019-06-17 20:02:46 +0300

    Implement card draw

--- src/main/java/tcgserver/Player.java
+++ src/main/java/tcgserver/Player.java
@@ -1,25 +1,47 @@
 package tcgserver;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class Player {
-    private List<Card> deck;
-    private List<Card> hand;
+    private final int MAX_HAND_SIZE = 5;
+    private ArrayList<Card> deck;
+    private ArrayList<Card> hand;
 
     public Player(List<Card> deck, List<Card> hand) {
-        this.deck = deck;
-        this.hand = hand;
+        this.deck = new ArrayList<>();
+        this.deck.addAll(deck);
+        this.hand = new ArrayList<>();
+        this.hand.addAll(hand);
     }
 
     public List<Card> getDeck() {
-        return deck;
+        return Collections.unmodifiableList(deck);
     }
 
     public List<Card> getHand() {
-        return hand;
+        return Collections.unmodifiableList(hand);
     }
 
     public boolean drawRandomCard() {
-        return false;
+        assert deck != null;
+        assert hand != null;
+
+        if (deck.size() < 1) {
+            return false;
+        }
+
+        Random random = new Random();
+        int randomCardIndex = random.nextInt(deck.size());
+        Card randomCard = deck.get(randomCardIndex);
+        deck.remove(randomCardIndex);
+
+        if (hand.size() < MAX_HAND_SIZE) {
+            hand.add(randomCard);
+        }
+
+        return true;
     }
 }
