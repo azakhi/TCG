cb125cce27b80d6f6478b9b6f04aa85fd150d38d
azakhi <azak.halilibrahim@gmail.com>
2019-06-17 19:50:47 +0300

    Add card draw tests

--- src/main/java/tcgserver/Player.java
+++ src/main/java/tcgserver/Player.java
@@ -1,5 +1,25 @@
 package tcgserver;
 
-public class Player {
+import java.util.List;
 
+public class Player {
+    private List<Card> deck;
+    private List<Card> hand;
+
+    public Player(List<Card> deck, List<Card> hand) {
+        this.deck = deck;
+        this.hand = hand;
+    }
+
+    public List<Card> getDeck() {
+        return deck;
+    }
+
+    public List<Card> getHand() {
+        return hand;
+    }
+
+    public boolean drawRandomCard() {
+        return false;
+    }
 }
--- src/test/java/tcgserver/PlayerTest.java
+++ src/test/java/tcgserver/PlayerTest.java
@@ -4,6 +4,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
@@ -14,5 +18,50 @@ public class PlayerTest {
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void drawRandomCard_CardAvailableNoCardsOnHand_CardDrawn() {
+        // Arrange
+        List<Card> deck = Arrays.asList(new Card(), new Card(), new Card());
+        Player player = new Player(deck, Collections.emptyList());
+
+        // Act
+        boolean isDrawn = player.drawRandomCard();
+
+        // Assert
+        assertEquals(true, isDrawn);
+        assertEquals(2, player.getDeck().size());
+        assertEquals(1, player.getHand().size());
+    }
+
+    @Test
+    public void drawRandomCard_NoCardsOnDeck_NoCardDrawn() {
+        // Arrange
+        Player player = new Player(Collections.emptyList(), Collections.emptyList());
+
+        // Act
+        boolean isDrawn = player.drawRandomCard();
+
+        // Assert
+        assertEquals(false, isDrawn);
+        assertEquals(0, player.getDeck().size());
+        assertEquals(0, player.getHand().size());
+    }
+
+    @Test
+    public void drawRandomCard_CardAvailableMaxCardsOnHand_CardDiscarded() {
+        // Arrange
+        List<Card> deck = Arrays.asList(new Card(), new Card(), new Card());
+        List<Card> hand = Arrays.asList(new Card(), new Card(), new Card(), new Card(), new Card());
+        Player player = new Player(deck, hand);
+
+        // Act
+        boolean isDrawn = player.drawRandomCard();
+
+        // Assert
+        assertEquals(true, isDrawn);
+        assertEquals(2, player.getDeck().size());
+        assertEquals(5, player.getHand().size());
     }
 }
